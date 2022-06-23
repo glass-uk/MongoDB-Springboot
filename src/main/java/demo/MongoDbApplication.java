eae6c5d75699cc5e58e5ea8a6c8d30a5f9e27b0e
Glass <Glass>
2022-06-23 22:20:59 +0100

    Glass - Classes

---
+++ src/main/java/demo/MongoDbApplication.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories("repository")
+public class MongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbApplication.class, args);
+	}
+
+}
